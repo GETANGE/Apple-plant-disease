f79e4aa9246d38e06e92e1badd71c8565e8f0317
Getange <emmanuelgetange48@gmail.com>
2024-07-02 00:21:57 +0300

    ADD: code refactoring

--- controllers/authController.js
+++ controllers/authController.js
@@ -191,9 +191,7 @@ exports.forgotPassword = async function(req, res, next) {
         validateBeforeSave: false
     })
 
-    const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`
-
-    const message = `Forgot your password? Submit  your new password and confirm your new password to : ${resetURL}.\n 
+    const message = `Your password reset token is  :\n  ${resetToken}.\n 
     If you did not make this request, please ignore this email and your password will remain unchanged.`
 
     try{
